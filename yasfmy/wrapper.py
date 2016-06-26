c73534c11a3bf054eaed2457a19b371256c07b28
yasufumi <yasufumi.taniguchi@gmail.com>
2016-06-26 17:30:36 +0900

    update wrapper.py

--- yasfmy/wrapper.py
+++ yasfmy/wrapper.py
@@ -1,7 +1,13 @@
 from functools import wraps
 
 from chainer import cuda
+from chainer import Variable as V
 import numpy as np
+
+def generate_variabled_method(xp, name):
+    def variabled(*args, volatile='OFF', **kwargs):
+        return V(xp.__dict__[name](*args, **kwargs), volatile=volatile)
+    return variabled
 
 class xpmeta(type):
     def __new__(cls, clsname, bases, clsdict):
@@ -10,6 +16,9 @@ class xpmeta(type):
             xp = cuda.cupy
         except RuntimeError:
             xp = np
+        methods = ['array', 'zeros']
+        for name in methods:
+            setattr(xp, name.capitalize(),  generate_variabled_method(xp, name))
         return xp
 
 class xp(metaclass=xpmeta):
