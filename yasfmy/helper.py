febacdfcca76078838f6df34200b2b7cc35900c9
yasufumi <yasufumi.taniguchi@gmail.com>
2016-06-29 11:40:48 +0900

    add helper.py

---
+++ yasfmy/helper.py
@@ -0,0 +1,13 @@
+from functools import wraps
+import time
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        elapsed = end - start
+        print('Elapsed time: %2.2f sec' % elapsed)
+        return result
+    return wrapper
