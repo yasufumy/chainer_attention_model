af1b6404d6dddfb874b0415fcfa83fbdec6796d5
yasufumi <yasufumi.taniguchi@gmail.com>
2016-06-30 09:48:04 +0900

    add test code for vocabulary.py

---
+++ tests/test_vocabulary.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append('..')
+import unittest
+from vocabulary import Vocabulary
+
+class VocabularyTestCode(unittest.TestCase):
+    def test_new(self):
+        words = ['a', 'b', 'c', 'a', 'a']
+        vocab = Vocabulary(words, th=2)
+        self.assertEqual(len(vocab), 4)
+        self.assertIn('a', vocab.wtoi)
