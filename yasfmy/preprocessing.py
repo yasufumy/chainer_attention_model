fa34c09533afb14d5c301c59854a941106421270
yasufumi <yasufumi.taniguchi@gmail.com>
2016-07-02 02:46:10 +0900

    update preprocessing.py

--- yasfmy/preprocessing.py
+++ yasfmy/preprocessing.py
@@ -10,25 +10,25 @@ def gen_lines(filename):
         for line in f:
             yield line.split()
 
-def gen_batch(lines, vocab, batch_size):
+def line2batch(lines, vocab, batch_size):
     batch = []
     wtoi = vocab.wtoi
     for line in lines:
         batch.append([wtoi['<s>']]+
                 [wtoi[word] for word in line] + [wtoi['</s>']])
         if len(batch) == batch_size:
-            yield fill_batch(batch)
+            #yield fill_batch(batch)
+            yield [V(word) for word in fill_batch(batch).T]
             batch = []
     if batch:
-        yield fill_batch(batch)
-
-def gen_word(batch):
-    batch_len = len(batch)
-    line_len = len(batch[0])
-    for l in range(line_len):
-        yield V(xp.array([batch[k][l] for k in range(batch_len)],
-                        dtype=xp.int32))
+        yield [V(word) for word in fill_batch(batch).T]
 
 def fill_batch(batch, token=-1):
     max_len = max(len(x) for x in batch)
-    return [x + [token] * (max_len - len(x) + 1) for x in batch]
+    return xp.array([x + [token] * (max_len - len(x) + 1) for x in batch], dtype=xp.int32)
+
+def batch2line(batches, vocab):
+    itow = vocab.itow
+    id_list = [batch.data.argmax(axis=1) for batch in batches]
+    for i in range(len(batches)):
+        yield ' '.join([itow[int(id_list[k][i])] for k in range(len(id_list))])
