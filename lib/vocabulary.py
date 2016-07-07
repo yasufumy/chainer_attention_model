f179e746402ee60940340fc7d6f9584dbc318b8d
yasufumi <yasufumi.taniguchi@gmail.com>
2016-07-07 16:08:01 +0900

    update Vocabulary class

--- lib/vocabulary.py
+++ lib/vocabulary.py
@@ -4,35 +4,27 @@ from itertools import chain, takewhile
 from config import START_TOKEN, END_TOKEN, UNKNOWN_TOKEN
 
 class Vocabulary:
-    def __new__(cls, words_generator, th):
+    def __new__(cls, words_generator, th=5):
         obj = super().__new__(cls)
 
         word_freq = Counter(chain.from_iterable((words_generator)))
 
-        obj.__wtoi = defaultdict(lambda: 0)
-        obj.__wtoi[UNKNOWN_TOKEN] = 0
-        obj.__wtoi[START_TOKEN] = 1
-        obj.__wtoi[END_TOKEN] = 2
-        obj.__itow = []
-        obj.__itow.append(UNKNOWN_TOKEN)
-        obj.__itow.append(START_TOKEN)
-        obj.__itow.append(END_TOKEN)
+        obj.wtoi = defaultdict(lambda: 0)
+        obj.wtoi[UNKNOWN_TOKEN] = 0
+        obj.wtoi[START_TOKEN] = 1
+        obj.wtoi[END_TOKEN] = 2
+        obj.itow = []
+        obj.itow.append(UNKNOWN_TOKEN)
+        obj.itow.append(START_TOKEN)
+        obj.itow.append(END_TOKEN)
 
         i = 3
         for (k, v) in takewhile(lambda x: x[1] > th, word_freq.most_common()):
-            obj.__wtoi[k] = i
-            obj.__itow.append(k)
+            obj.wtoi[k] = i
+            obj.itow.append(k)
             i += 1
 
         return obj
 
     def __len__(self):
         return len(self.itow)
-
-    @property
-    def wtoi(self):
-        return self.__wtoi
-
-    @property
-    def itow(self):
-        return self.__itow
